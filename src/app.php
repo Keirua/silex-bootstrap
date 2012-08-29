ca9bac57de07c2e27745055158e787115319342d
Keirua <keirua@gmail.com>
2012-08-29 16:20:37 +0200

    modified gitignore

--- src/app.php
+++ src/app.php
@@ -15,3 +15,15 @@ $app->match('/hello/{name}', function($name) use ($app) {
 })->bind('kp_hello')
 ->value('name', 'plop')
 ;
+
+$app->error(function (\Exception $e, $code) {
+    switch ($code) {
+        case 404:
+            $message = 'The requested page could not be found.';
+            break;
+        default:
+            $message = 'We are sorry, but something went terribly wrong.';
+    }
+
+    return new Response($message, $code);
+});
